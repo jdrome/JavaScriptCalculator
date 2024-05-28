3651c639b0ec19a38422250ea2dc5dced2a846e2
Jose D. Romero <josedanilromero@gmail.com>
2024-05-28 16:24:16 -0500

    Commit 19: Declared append, clear, and calculate functions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Calculator Program
+
+const display = document.getElementByUd("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+
+}
+
+function calculate() {
+
+}
